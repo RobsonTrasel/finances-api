48a71eeb3995da78c0a5f784bb38cbf34aff3079
Robson Trasel <robson.trasel@gmail.com>
2022-12-17 00:41:14 -0300

    feat: transaction resource created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
